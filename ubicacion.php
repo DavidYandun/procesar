0a1f1d01ae6f94557ec511bb7700d15cca5a8954
DavidYandun <davidcad963@gmail.com>
2018-07-11 01:57:03 -0500

    asdasd

---
+++ ubicacion.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mapa básico</title>
+<style>
+	/*Estilos para el contenedor del mapa*/
+	#map_lima{
+		width:100%px;
+		height:650px;
+	}
+</style>
+<!-- Agregar la librería de Google Maps API -->
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+// Función para inicializar el mapa
+function inicializar() {
+  //Variable que almacena las coordenadas
+var myCenter=new google.maps.LatLng(0.36128,-78.1113);
+var myCenter1=new google.maps.LatLng(0.36028,-78.1113);
+  var mapa_lima = {
+  	// Mostramos las coordenadas (Latitud, Longitud) en el centro del mapa
+    center:new google.maps.LatLng(0.36028,-78.1113),
+    // Tamaño del zoom
+    zoom:15,
+    // Tipo de mapa: ROADMAP, SATELLITE, HYBRID, TERRAIN 
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  // Creamos un mapa en el contenedor con id map_lima,  usando los parámetros de la variable mapa_lima
+  var map=new google.maps.Map(document.getElementById("map_lima"), mapa_lima);
+
+  //Mostramos el marcador en las coordenadas almacenada en la variable myCenter
+var marker=new google.maps.Marker({
+  position:myCenter,
+  
+  });
+  var marker1=new google.maps.Marker({
+  position:myCenter1,
+  
+  });
+
+//Añadimos el marcador para el mapa utilizando el método setMap()
+marker.setMap(map);
+marker1.setMap(map);
+
+}
+
+
+//Mostramos el mapa una vez que cargue la página, con el evento addDomListener de Google Maps API
+google.maps.event.addDomListener(window, 'load', inicializar);
+</script>
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=
+AIzaSyBReOhtkHkZPdOqYIan2P0mQzlGsEBuASw"></script>
+</head>
+<body>
+<!-- Contenedor del mapa -->
+<div id="map_lima"></div>
+</body>
+</html>
